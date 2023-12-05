b526451701ffcb054710d63733705d62dacf6ed7
TonyLig <118136260+TonyLig@users.noreply.github.com>
2023-12-05 23:12:23 +0100

    add card

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
+import { ParallaxProvider } from "react-scroll-parallax";
+
 import "./App.css";
 import Banner from "./assets/componets/Banner";
-import { ParallaxProvider } from "react-scroll-parallax";
+import Card from "./assets/componets/Card";
+import temperature from ".//assets/img/high-temperatures.jpg";
 
 export default function App() {
   return (
     <>
       <ParallaxProvider>
         <Banner />
-        <div
-          className="h-screen flex items-center justify-center"
-          id="card-section"
-        >
-          <h1 className="headline gray">Goodnight.</h1>
+        <div className="h-screen grid grid-cols-4 gap-4" id="card-section">
+          <Card
+            pageLink=""
+            imageCard={temperature}
+            titleCard="TEMPERATURE"
+            accessibilityDescription="image of the sun"
+          />
+          <Card
+            pageLink=""
+            imageCard={temperature}
+            titleCard="TEMPERATURE"
+            accessibilityDescription="image of the sun"
+          />
+          <Card
+            pageLink=""
+            imageCard={temperature}
+            titleCard="TEMPERATURE"
+            accessibilityDescription="image of the sun"
+          />
+          <Card
+            pageLink=""
+            imageCard={temperature}
+            titleCard="TEMPERATURE"
+            accessibilityDescription="image of the sun"
+          />
+          <Card
+            pageLink=""
+            imageCard={temperature}
+            titleCard="TEMPERATURE"
+            accessibilityDescription="image of the sun"
+          />
         </div>
       </ParallaxProvider>
     </>
--- src/assets/componets/Card.tsx
+++ src/assets/componets/Card.tsx
@@ -1,3 +1,30 @@
-export default function Card() {
-  return <div></div>;
+interface Props {
+  imageCard: string;
+  pageLink: string;
+  accessibilityDescription: string;
+  titleCard: string;
+}
+
+export default function Card({
+  imageCard,
+  pageLink,
+  accessibilityDescription,
+  titleCard,
+}: Props) {
+  return (
+    <div className=" rounded-xl shadow-lg shadow-neutral-900 md:shadow-xl md:shadow-neutral-900  relative overflow-hidden bg-cover bg-no-repeat">
+      <img
+        className="rounded-xl"
+        src={imageCard}
+        alt={accessibilityDescription}
+      />
+      <a href={pageLink}>
+        <div className="flex items-center justify-center absolute inset-0 h-full w-full overflow-hidden backdrop-blur-md bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+          <h1 className="text-3xl text-slate-100 text font-sans font-bold p-4 rounded-xl">
+            {titleCard}
+          </h1>
+        </div>
+      </a>
+    </div>
+  );
 }
